0d86f451fa500fbd96bc155ef7f7f6d9001b6488
akqiku@assura.ch <akqiku@assura.ch>
2024-04-01 10:31:23 +0000

    Finished models in dev mode

---
+++ models/staging/snapshot_streamed_data.sql
@@ -0,0 +1,19 @@
+{{
+    config(
+        materialized='table'
+        )
+}}
+
+select
+    company_name,
+    company_number,
+    -- company_status,
+    concat("test_", current_timestamp()) as company_status,
+    date_of_creation,
+    postal_code,
+    stream.published_at
+from
+    {{ source("staging", "company_house_stream") }} stream
+join 
+    {{ source("development", "get_last_timestamp") }} last_timestamp
+    on stream.published_at > last_timestamp.published_at
